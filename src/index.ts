c6f8d1801382c48c5767407185cb3001fc238ec8
partiksingh1 <20btrcs105@jainuniversity.ac.in>
2024-11-04 15:14:56 +0530

    awake

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import adminRouter from './route/adminRoute';
 import authRoute from './route/authRoute';
 import salespersonRoute from './route/SalespersonRoute';
 import distributorRoute from './route/distributorsRoute';
+import https from 'https'; 
 
 const app: Express = express();
 const port: number = 3000;
@@ -18,6 +19,14 @@ app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/salesperson", salespersonRoute);
 app.use("/api/v1/distributor", distributorRoute);
+const keepAlive = () => {
+    https.get('https://speedsales.onrender.com/api/v1', (res) => {
+        console.log(`Keep-alive pinged: ${res.statusCode}`);
+    }).on('error', (err) => {
+        console.error(`Error pinging: ${err.message}`);
+    });
+};
+setInterval(keepAlive, 10 * 60 * 1000);
 
 // Start the server
 app.listen(port, () => {
