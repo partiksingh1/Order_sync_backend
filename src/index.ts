129c7a4a7a4abd5a2f7760edb6979ca8d5401e68
partiksingh1 <20btrcs105@jainuniversity.ac.in>
2024-11-08 20:51:41 +0530

    index.ts added

--- src/index.ts
+++ src/index.ts
@@ -19,16 +19,15 @@ app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/salesperson", salespersonRoute);
 app.use("/api/v1/distributor", distributorRoute);
-// const keepAlive = () => {
-//     https.get('https://speedsales.onrender.com/api/v1', (res) => {
-//         console.log(`Keep-alive pinged: ${res.statusCode}`);
-//     }).on('error', (err) => {
-//         console.error(`Error pinging: ${err.message}`);
-//     });
-// };
-// setInterval(keepAlive, 10 * 60 * 1000);
+const keepAlive = () => {
+    https.get('https://speedsales.onrender.com/api/v1', (res) => {
+        console.log(`Keep-alive pinged: ${res.statusCode}`);
+    }).on('error', (err) => {
+        console.error(`Error pinging: ${err.message}`);
+    });
+};
+setInterval(keepAlive, 10 * 60 * 1000);
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
